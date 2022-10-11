e2d0b9ec108a94afc16ad5ca7156630a75965d04
Mahmoudsallam9 <63003642+Mahmoudsallam9@users.noreply.github.com>
2022-10-11 23:57:58 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,21 @@
+/*============================= Typing =============================*/
+var typed = new Typed(".typing", {
+    strings: ["Developer", "Chemical Engineer", "Designer", "Photographer"],
+    typeSpeed: 100,
+    backSpeed: 40,
+    loop: true
+});
+/*============================= Aside =============================*/
+const nav = document.querySelector(".nav"),
+    navList = nav.querySelectorAll("li"),
+    totalNavList = navList.length
+for (let i = 0; i < totalNavList; i++) {
+
+    const a = navList[i].querySelector("a");
+    a.addEventListener("click", function () {
+        for (let j = 0; j < totalNavList; j++) {
+            navList[j].querySelector("a").classList.remove("active")
+        }
+        this.classList.add("active")
+    })
+}
